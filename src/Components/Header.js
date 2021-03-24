b35c4c504a6fe878cfa04bdd1e81994fe29a6b0c
Ivo Mujo <ivo.mujo.3@gmail.com>
2021-03-24 08:28:48 +0100

    test

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
         <div className='headerDiv'>
             <div className="title">
                 <h1>PRIPREMI</h1>
-                <h1 className='titleNastavak'>SE</h1>
+                <h1 className='titleNastavak'>ME</h1>
             </div>
         </div>
     )
