b1b6e19b9ccb369833b7127d914b81e653228ee7
Ivo Mujo <ivo.mujo.3@gmail.com>
2021-03-26 17:32:22 +0100

    Adding card image animation

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,10 +1,35 @@
 import CardTitlePicture from './CardTitlePicture'
 import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+import { useState } from 'react';
+
 
 const Card = () => {
+    const [resize, setResize] = useState(0)
+    const [isactive, setIsActive] = useState(0)
+
+    function handleClick() {
+        setResize(1)
+        if (isactive===0){
+            setIsActive(1)
+        }else{
+            setIsActive(0)
+        }
+        
+        
+    }
+
     return(
         <div className='cardDiv'>
-            <CardTitlePicture />
+            <div 
+                className='imageAnimation' 
+                onClick={handleClick}
+                onAnimationEnd={()=>setResize(0)}
+                resize={resize}
+                isactive={isactive}
+                black='black'
+            >
+                <CardTitlePicture/>
+            </div>
         </div>
     )
 }
